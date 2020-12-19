961278cfa26fbc7f0dcb6f8c1c48d3af68c3ee3c
samuel <sami_zar@hotmail.com>
2020-12-18 18:32:28 -0600

    modificacion de user.js

--- user.js
+++ user.js
@@ -2,11 +2,12 @@
   const express = require('express');
   var UserController=require('../controllers/user');
   var md_auth=require('../middlewares/authenticated');
-  
+
   var multipart = require('connect-multiparty');
   var md_upload=multipart({uploadDir:'./uploads/users'});
 
   var api=express.Router();
+    console.log("this is my fist commit since client");
 
   api.get('/probando-controlador', md_auth.ensureAuth ,UserController.pruebas);
   api.post('/register',UserController.SaveUser);
